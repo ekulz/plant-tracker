80b425d36e57c5ad14d07a16aa857aa6b6a1f332
ekulz <lukehickin@hotmail.com>
2020-08-13 00:22:36 -0700

    Add placeholder plant and ability to add schedules

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <div class="plant">
+        <div class="title">Lily</div>
+        <div class="notes">Peace Lily</div>
+        <div class="schedule">
+          <ul>
+            <li v-for="schedule in schedules" v-bind:key="schedule">
+              {{ schedule }}
+            </li>
+          </ul>
+        </div>
+        <div class="add-schedule">
+          <input
+            type="text"
+            class="new-schedule"
+            placeholder="Search..."
+            v-model="entry"
+          />
+          <button class="save-schedule" v-on:click="saveSchedule">Save</button>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function() {
+    return {
+      schedules: ["Fertilized 11/08", "Watered 12/08"],
+      entry: "",
+    };
+  },
+  methods: {
+    saveSchedule() {
+      if (this.entry) this.schedules.push(this.entry);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #181a1b;
+  color: #fff;
+}
+
+main {
+  padding: 25px;
+  margin-left: 20%;
+  margin-right: 20%;
+}
+
+.plant {
+  background-color: grey;
+}
+
+.plant .title {
+  font-size: 32px;
+  font-weight: 500;
+  margin: 10px;
+  padding-top: 10px;
+}
+
+.plant .notes {
+  font-size: 20px;
+  font-style: italic;
+  margin: 5px;
+}
+
+.plant .add-schedule {
+  margin: 5px;
+  padding-bottom: 5px;
 }
 </style>
