dabb98eacb1e21ad5c06b14c4738693019d36843
ekulz <lukehickin@hotmail.com>
2020-08-23 13:06:26 -0700

    Add ability to add new plant

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,40 @@
 <template>
   <div id="app">
-    <main class="flex justify-center">
-      <div id="plant" class="bg-gray-800 text-gray-500 w-2/5 rounded-lg mt-4">
-        <div id="name" class="text-4xl text-gray-400 italic m-4">
-          Lily
-        </div>
-        <div id="species" class="text-xl m-4">Peace Lily</div>
+    <main class="flex flex-col items-center">
+      <div
+        id="plant"
+        class="bg-gray-800 text-gray-500 w-2/5 rounded-lg mt-4"
+        v-for="plant in plants"
+        v-bind:key="plant.id"
+      >
+        <div id="name" class="text-4xl text-gray-400 italic m-4">{{ plant.name }}</div>
+        <div id="species" class="text-xl m-4">{{ plant.species }}</div>
         <div id="logs" class="text-base text-gray-400 m-4">
           <ul class="list-disc list-inside">
-            <li v-for="log in logs" v-bind:key="log">
-              {{ log }}
-            </li>
+            <li v-for="log in plant.logs" v-bind:key="log">{{ log }}</li>
           </ul>
         </div>
         <div id="add-log" class="flex justify-left m-4">
           <input
             type="text"
             id="new-log"
-            class="bg-gray-700 focus:outline-none focus:shadow-outline border border-gray-700 rounded-lg w-1/3 py-1 px-2 appearance-none leading-normal text-gray-300"
+            class="bg-gray-700 focus:outline-none focus:shadow-outline border border-gray-700 rounded-lg w-1/2 py-1 px-2 appearance-none leading-normal text-gray-300"
             placeholder="e.g. Watered 01/12/20..."
-            v-model="logEntry"
+            v-model="plant.logEntry"
           />
           <button
             id="save-log"
             class="bg-gray-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-lg ml-2"
-            v-on:click="saveLog"
-          >
-            Save
-          </button>
+            v-on:click="saveLog(plant.id)"
+          >Save</button>
         </div>
+      </div>
+      <div id="new-plant-container" class="w-2/5 mt-4">
+        <button
+          id="new-plant-btn"
+          class="bg-gray-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-lg"
+          v-on:click="addPlant()"
+        >Add plant</button>
       </div>
     </main>
   </div>
@@ -37,15 +43,35 @@
 <script>
 export default {
   name: "App",
-  data: function() {
+  data: function () {
     return {
-      logs: ["Fertilized 11/08", "Watered 12/08"],
-      logEntry: "",
+      plants: [
+        {
+          id: 1,
+          name: "Lily",
+          species: "Peace Lily",
+          logs: ["Fertilized 11/08", "Watered 12/08"],
+          logEntry: "",
+        },
+      ],
     };
   },
   methods: {
-    saveLog() {
-      if (this.logEntry) this.logs.push(this.logEntry);
+    saveLog(plantId) {
+      let plant = this.plants.find((x) => x.id === plantId);
+      if (plant.logEntry) plant.logs.push(plant.logEntry);
+      plant.logEntry = "";
+    },
+    addPlant() {
+      let lastId = Math.max(...this.plants.map((plant) => plant.id));
+      let plant = {
+        id: ++lastId,
+        name: "",
+        species: "",
+        logs: [],
+        logEntry: "",
+      };
+      this.plants.push(plant);
     },
   },
 };
