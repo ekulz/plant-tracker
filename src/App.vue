03e0e58293d0082bec45369ee6f6746632867322
ekulz <lukehickin@hotmail.com>
2020-08-23 00:12:51 -0700

    Update custom styles to use tailwind styles

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <template>
   <div id="app">
-    <main>
-      <div class="plant">
-        <div class="title">Lily</div>
-        <div class="notes">Peace Lily</div>
-        <div class="schedule">
-          <ul>
-            <li v-for="schedule in schedules" v-bind:key="schedule">
-              {{ schedule }}
+    <main class="flex justify-center">
+      <div id="plant" class="bg-gray-800 text-gray-500 w-2/5 rounded-lg mt-4">
+        <div id="name" class="text-4xl text-gray-400 italic m-4">
+          Lily
+        </div>
+        <div id="species" class="text-xl m-4">Peace Lily</div>
+        <div id="logs" class="text-base text-gray-400 m-4">
+          <ul class="list-disc list-inside">
+            <li v-for="log in logs" v-bind:key="log">
+              {{ log }}
             </li>
           </ul>
         </div>
-        <div class="add-schedule">
+        <div id="add-log" class="flex justify-left m-4">
           <input
             type="text"
-            class="new-schedule"
-            placeholder="Search..."
-            v-model="entry"
+            id="new-log"
+            class="bg-gray-700 focus:outline-none focus:shadow-outline border border-gray-700 rounded-lg w-1/3 py-1 px-2 appearance-none leading-normal text-gray-300"
+            placeholder="e.g. Watered 01/12/20..."
+            v-model="logEntry"
           />
-          <button class="save-schedule" v-on:click="saveSchedule">Save</button>
+          <button
+            id="save-log"
+            class="bg-gray-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-lg ml-2"
+            v-on:click="saveLog"
+          >
+            Save
+          </button>
         </div>
       </div>
     </main>
@@ -30,13 +39,13 @@ export default {
   name: "App",
   data: function() {
     return {
-      schedules: ["Fertilized 11/08", "Watered 12/08"],
-      entry: "",
+      logs: ["Fertilized 11/08", "Watered 12/08"],
+      logEntry: "",
     };
   },
   methods: {
-    saveSchedule() {
-      if (this.entry) this.schedules.push(this.entry);
+    saveLog() {
+      if (this.logEntry) this.logs.push(this.logEntry);
     },
   },
 };
@@ -44,35 +53,6 @@ export default {
 
 <style>
 body {
-  background-color: #181a1b;
-  color: #fff;
-}
-
-main {
-  padding: 25px;
-  margin-left: 20%;
-  margin-right: 20%;
-}
-
-.plant {
-  background-color: grey;
-}
-
-.plant .title {
-  font-size: 32px;
-  font-weight: 500;
-  margin: 10px;
-  padding-top: 10px;
-}
-
-.plant .notes {
-  font-size: 20px;
-  font-style: italic;
-  margin: 5px;
-}
-
-.plant .add-schedule {
-  margin: 5px;
-  padding-bottom: 5px;
+  @apply bg-gray-900;
 }
 </style>
