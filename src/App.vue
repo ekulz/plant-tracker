51794e233cbcf1bde2bc9f7dae32a83b35451a39
ekulz <lukehickin@hotmail.com>
2020-08-23 17:45:07 -0700

    Add ability to add name and species to new plant

--- src/App.vue
+++ src/App.vue
@@ -29,12 +29,56 @@
           >Save</button>
         </div>
       </div>
-      <div id="new-plant-container" class="w-2/5 mt-4">
-        <button
-          id="new-plant-btn"
-          class="bg-gray-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-lg"
-          v-on:click="addPlant()"
-        >Add plant</button>
+      <div id="new-plant-container" class="flex flex-col w-2/5 mt-4">
+        <div
+          id="new-plant-edit"
+          class="bg-gray-800 text-gray-500 rounded-lg"
+          v-if="newPlant.editing"
+        >
+          <div id="new-plant-edit-name" class="m-4 flex flex-row items-center">
+            <div class="w-1/6">
+              <label for="name-input" class="text-gray-400">Name</label>
+            </div>
+            <div class="w-full">
+              <input
+                type="text"
+                id="name-input"
+                class="bg-gray-700 focus:outline-none focus:shadow-outline border border-gray-700 rounded-lg py-1 px-2 appearance-none leading-normal text-gray-300 w-full"
+                placeholder="e.g. Lily"
+                v-model="newPlant.name"
+              />
+            </div>
+          </div>
+          <div id="new-plant-edit-species" class="m-4 flex flex-row items-center">
+            <div class="w-1/6">
+              <label for="species-input" class="text-gray-400">Species</label>
+            </div>
+            <div class="w-full">
+              <input
+                type="text"
+                id="species-input"
+                class="bg-gray-700 focus:outline-none focus:shadow-outline border border-gray-700 rounded-lg py-1 px-2 appearance-none leading-normal text-gray-300 w-full"
+                placeholder="e.g. Peace Lily"
+                v-model="newPlant.species"
+              />
+            </div>
+          </div>
+          <div id="new-plant-edit-save" class="m-4">
+            <button
+              id="new-plant-edit-save-btn"
+              class="bg-gray-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-lg"
+              v-on:click="savePlant()"
+            >Save</button>
+          </div>
+        </div>
+        <div id="new-plant-add">
+          <button
+            id="new-plant-add-btn"
+            class="bg-gray-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-lg"
+            v-if="!newPlant.editing"
+            v-on:click="toggleEdit()"
+          >Add plant</button>
+        </div>
       </div>
     </main>
   </div>
@@ -54,6 +98,11 @@ export default {
           logEntry: "",
         },
       ],
+      newPlant: {
+        editing: false,
+        name: "",
+        species: "",
+      },
     };
   },
   methods: {
@@ -62,16 +111,22 @@ export default {
       if (plant.logEntry) plant.logs.push(plant.logEntry);
       plant.logEntry = "";
     },
-    addPlant() {
+    savePlant() {
       let lastId = Math.max(...this.plants.map((plant) => plant.id));
       let plant = {
         id: ++lastId,
-        name: "",
-        species: "",
+        name: this.newPlant.name,
+        species: this.newPlant.species,
         logs: [],
         logEntry: "",
       };
       this.plants.push(plant);
+      this.newPlant.name = "";
+      this.newPlant.species = "";
+      this.toggleEdit();
+    },
+    toggleEdit() {
+      this.newPlant.editing = !this.newPlant.editing;
     },
   },
 };
