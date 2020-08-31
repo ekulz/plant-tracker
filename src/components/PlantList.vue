2efcb633f0a4cb35b3722e3d5757b1440610e3bf
ekulz <lukehickin@hotmail.com>
2020-08-30 21:54:43 -0700

    Extract plants into plant list item component

--- src/components/PlantList.vue
+++ src/components/PlantList.vue
@@ -1,59 +1,23 @@
 <template>
   <div class="container container-content">
-    <div class="card w-3/5" v-for="plant in plants" v-bind:key="plant.id">
-      <div class="h1">{{ plant.name }}</div>
-      <div class="h2">{{ plant.species }}</div>
-      <div class="p">
-        <ul class="list-disc list-inside">
-          <li v-for="log in plant.logs" v-bind:key="log">{{ log }}</li>
-        </ul>
-      </div>
-      <div class="flex m-4">
-        <input
-          type="text"
-          class="input w-full"
-          placeholder="e.g. Watered 01/12/20..."
-          v-model="plant.logEntry"
-        />
-        <button class="btn ml-2" v-on:click="saveLog(plant.id)">Save</button>
-      </div>
-    </div>
+    <plant-list-item
+      v-for="plant in plants"
+      v-bind:plant="plant"
+      v-bind:key="plant.id"
+    ></plant-list-item>
   </div>
 </template>
 
 <script>
+import PlantListItem from "./PlantListItem.vue";
+
 export default {
   name: "plant-list",
   props: {
     plants: Array,
   },
-  methods: {
-    saveLog(plantId) {
-      let plant = this.plants.find((x) => x.id === plantId);
-      if (plant.logEntry) plant.logs.push(plant.logEntry);
-      plant.logEntry = "";
-    },
+  components: {
+    PlantListItem,
   },
 };
 </script>
-
-<style>
-.h1 {
-  @apply text-4xl;
-  @apply text-gray-400;
-  @apply italic;
-  @apply m-4;
-}
-
-.h2 {
-  @apply text-xl;
-  @apply text-gray-500;
-  @apply m-4;
-}
-
-.p {
-  @apply text-base;
-  @apply text-gray-400;
-  @apply m-4;
-}
-</style>
---
+++ src/components/PlantListItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="card w-3/5">
+    <div class="h1">{{ plant.name }}</div>
+    <div class="h2">{{ plant.species }}</div>
+    <div class="list">
+      <ul class="list-disc list-inside">
+        <li v-for="log in plant.logs" v-bind:key="log">{{ log }}</li>
+      </ul>
+    </div>
+    <div class="flex m-4">
+      <input
+        type="text"
+        class="input w-full"
+        placeholder="e.g. Watered 01/12/20..."
+        v-model="plant.logEntry"
+      />
+      <button class="btn ml-2" v-on:click="saveLog()">Save</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "plant-list-item",
+  props: {
+    plant: Object,
+  },
+  methods: {
+    saveLog() {
+      if (this.plant.logEntry) {
+        this.plant.logs.push(this.plant.logEntry);
+        this.plant.logEntry = "";
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.h1 {
+  @apply text-4xl;
+  @apply text-gray-400;
+  @apply italic;
+  @apply m-4;
+}
+
+.h2 {
+  @apply text-xl;
+  @apply text-gray-500;
+  @apply m-4;
+}
+
+.list {
+  @apply text-base;
+  @apply text-gray-400;
+  @apply m-4;
+}
+</style>
