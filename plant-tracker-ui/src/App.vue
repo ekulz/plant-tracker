33b2b242ce66eb2b4bf3e63419893ec43318859e
ekulz <lukehickin@hotmail.com>
2020-12-01 22:26:02 -0800

    Add vue router

--- plant-tracker-ui/src/App.vue
+++ plant-tracker-ui/src/App.vue
@@ -1,27 +1,5 @@
 <template>
   <div id="app">
-    <the-header></the-header>
-    <main>
-      <plant-list></plant-list>
-    </main>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import TheHeader from "./components/TheHeader.vue";
-import PlantList from "./components/PlantList.vue";
-
-export default {
-  name: "App",
-  components: {
-    TheHeader,
-    PlantList,
-  },
-};
-</script>
-
-<style>
-body {
-  @apply bg-gray-900;
-}
-</style>
---
+++ plant-tracker-ui/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <the-header></the-header>
+    <main>
+      <plant-list></plant-list>
+    </main>
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/TheHeader.vue";
+import PlantList from "@/components/PlantList.vue";
+
+export default {
+  name: 'Home',
+  components: {
+    TheHeader,
+    PlantList,
+  }
+}
+</script>
+
+<style>
+body {
+  @apply bg-gray-900;
+}
+</style>
