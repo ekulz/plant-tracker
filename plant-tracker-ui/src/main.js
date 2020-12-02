9eeb57a990a7463bce3bbcbb183abd06dcd5ebc1
ekulz <lukehickin@hotmail.com>
2020-12-01 23:22:11 -0800

    Add Auth0 login to dev env

--- plant-tracker-ui/src/main.js
+++ plant-tracker-ui/src/main.js
@@ -1,8 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
-
 import "@/assets/styles/index.css";
 import router from './router'
+import { domain, clientId } from "../auth_config.json";
+import { Auth0Plugin } from "./auth";
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
 
 Vue.config.productionTip = false;
 
--- plant-tracker-ui/src/views/Home.vue
+++ plant-tracker-ui/src/views/Home.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="home">
-    <the-header></the-header>
+    <the-header/>
     <main>
-      <plant-list></plant-list>
+        <div class="container container-content" v-if="!$auth.loading">
+        <button class="btn btn-primary m-2" v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+        <button class="btn btn-primary m-2" v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+      </div>
+      <plant-list/>
     </main>
   </div>
 </template>
@@ -16,6 +20,16 @@ export default {
   components: {
     TheHeader,
     PlantList,
+  },
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
   }
 }
 </script>
