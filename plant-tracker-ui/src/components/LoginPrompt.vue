739908a4c76d51d530e6da0b8d83302d5216fee3
ekulz <lukehickin@hotmail.com>
2020-12-19 22:33:29 -0800

    Add logo to login prompt

--- plant-tracker-ui/src/components/LoginPrompt.vue
+++ plant-tracker-ui/src/components/LoginPrompt.vue
@@ -1,19 +1,36 @@
 <template>
-  <p class="text">Welcome! Please login or create an account to get started.</p>
+  <div class="welcome-container">
+    <img class="welcome-image" :src="`${publicPath}logo.png`" alt="logo" width="200px">
+    <p class="welcome-text">Welcome! Please sign in or create an account to continue.</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "login-prompt",
+  data () {
+    return {
+      publicPath: process.env.BASE_URL
+    }
+  }
 };
 </script>
 
 <style>
-.text {
+.welcome-container {
+  @apply text-center;
+  @apply block;
+}
+
+.welcome-image {
+  @apply mt-16;
+  @apply inline;
+}
+
+.welcome-text {
   @apply text-lg;
   @apply text-gray-400;
-  @apply text-center;
   @apply italic;
-  @apply mt-12;
+  @apply mt-4;
 }
 </style>
